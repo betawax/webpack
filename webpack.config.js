de1d28dd9b313e03aa4d62a4075e707138a9a5d2
Holger Weis <holger.weis@gmail.com>
2020-09-06 15:14:50 +0200

    Add a minimal environment specific config

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,52 @@
 const path = require('path')
 const webpack = require('webpack')
+const { merge } = require('webpack-merge')
 
 //
 // Webpack
 //
 
-module.exports = {}
+module.exports = {
+
+  entry: {
+    'application': [
+      './resources/scripts/application.js'
+    ]
+  },
+
+  output: {
+    path: path.resolve(__dirname, 'public/dist'),
+    publicPath: 'dist/'
+  }
+
+}
+
+//
+// Development
+//
+
+if (process.env.NODE_ENV === 'development') {
+  module.exports = merge(module.exports, {
+
+    output: {
+      filename: 'scripts/[name].min.js',
+      chunkFilename: 'scripts/[name].min.js'
+    }
+
+  })
+}
+
+//
+// Production
+//
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports = merge(module.exports, {
+
+    output: {
+      filename: 'scripts/[name]-[contenthash:7].min.js',
+      chunkFilename: 'scripts/[name]-[chunkhash:7].min.js'
+    }
+
+  })
+}
