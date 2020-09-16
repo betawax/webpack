eaad4aa1096e2125a8a455d7443d6699e1f1c4d3
Holger Weis <holger.weis@gmail.com>
2020-09-16 09:15:24 +0200

    Clean the dist folder on each build

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const webpack = require('webpack')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const { merge } = require('webpack-merge')
 
 //
@@ -17,7 +18,11 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'public/dist'),
     publicPath: 'dist/'
-  }
+  },
+
+  plugins: [
+    new CleanWebpackPlugin()
+  ]
 
 }
 
